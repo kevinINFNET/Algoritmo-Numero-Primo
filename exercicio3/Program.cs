97454d942075da819d55ffdc19062816976644d7
kevin <kevin@XDXDXD>
2024-05-20 08:43:14 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        Console.Write("Entre com um número inteiro: ");
+        if (int.TryParse(Console.ReadLine(), out int numero))
+        {
+            if (EPrimo(numero))
+            {
+                Console.WriteLine($"{numero} é um número primo.");
+            }
+            else
+            {
+                Console.WriteLine($"{numero} não é um número primo.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida. Por favor, entre com um número inteiro.");
+        }
+    }
+
+    static bool EPrimo(int numero)
+    {
+        if (numero <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(numero); i++)
+        {
+            if (numero % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
